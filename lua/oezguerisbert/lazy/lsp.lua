6c0e21d3050b1d9a813771ac45bb7be6e20928cc
Özgür Isbert <oezguerisbert@gmail.com>
2024-04-18 15:29:42 +0200

    feat+chores: added prettier, lsp, autopairs

--- lua/oezguerisbert/lazy/lsp.lua
+++ lua/oezguerisbert/lazy/lsp.lua
@@ -3,18 +3,24 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lua",
+
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
+        "VonHeikemen/lsp-zero.nvim",
+        "rafamadriz/friendly-snippets"
     },
     config = function()
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
+        local lsp = require("lsp-zero")
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
@@ -73,6 +79,23 @@ return {
                 { name = 'buffer' },
             })
         })
+
+    lsp.on_attach(function(client, bufnr)
+      local opts = {buffer = bufnr, remap = false}
+
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+      vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    end)
+
+    lsp.setup()
         vim.diagnostic.config({
             -- update_in_insert = true,
             float = {
