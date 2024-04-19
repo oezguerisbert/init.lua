9e10a022631c9ecb0bc0f9b78431bf3547f3ed32
Özgür Isbert <oezguerisbert@gmail.com>
2024-04-19 10:30:09 +0200

    fix: treesitter was acting up, changed some settings

--- lua/oezguerisbert/lazy/treesitter.lua
+++ lua/oezguerisbert/lazy/treesitter.lua
@@ -11,7 +11,8 @@ return {
 
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-      auto_install = true,
+      auto_install = false,
+      prefer_git = false,
 
       indent = {
         enable = true
