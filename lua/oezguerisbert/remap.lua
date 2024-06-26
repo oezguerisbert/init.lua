ffb76fceb32ce854805b7ad7cf4b1ef4af5dce5e
Özgür Isbert <oezguerisbert@gmail.com>
2024-06-26 13:44:48 +0200

    updates+feat!: versions + nvim-tree

--- lua/oezguerisbert/remap.lua
+++ lua/oezguerisbert/remap.lua
@@ -9,6 +9,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("i", "<C-BS>", "<C-W>")
 
 --vim.keymap.set("n", "<leader>vwm", function()
 --    require("vim-with-me").StartVimWithMe()
