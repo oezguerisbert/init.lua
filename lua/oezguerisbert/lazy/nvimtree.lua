cc5fcf101b797f8454e064dcd26c2891171013bb
Özgür Isbert <oezguerisbert@gmail.com>
2024-06-26 14:16:30 +0200

    issues: nvim-tree doesnt work with harpoon and default vim keybinds

--- lua/oezguerisbert/lazy/nvimtree.lua
+++ lua/oezguerisbert/lazy/nvimtree.lua
@@ -1,11 +1,12 @@
-return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
-}
+return  {}
+--return {
+--  "nvim-tree/nvim-tree.lua",
+--  version = "*",
+--  lazy = false,
+--  dependencies = {
+--    "nvim-tree/nvim-web-devicons",
+--  },
+--  config = function()
+--    require("nvim-tree").setup {}
+--  end,
+--}
