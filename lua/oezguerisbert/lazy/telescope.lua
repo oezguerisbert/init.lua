b1765e8cf1bded27662876eff38d9d21c8b91903
Özgür Isbert <oezguerisbert@gmail.com>
2024-07-07 12:21:40 +0200

    chores

--- lua/oezguerisbert/lazy/telescope.lua
+++ lua/oezguerisbert/lazy/telescope.lua
@@ -7,22 +7,22 @@ return {
     require('telescope').setup({})
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files,
-    { noremap = true, silent = true }
+      { noremap = true, silent = true }
     )
     vim.keymap.set('n', '<leader>fg', builtin.live_grep,
-    { noremap = true, silent = true }
+      { noremap = true, silent = true }
     )
     vim.keymap.set('n', '<leader>fb', builtin.buffers,
-    { noremap = true, silent = true }
+      { noremap = true, silent = true }
     )
     vim.keymap.set('n', '<leader>fd', builtin.diagnostics,
-    { noremap = true, silent = true }
+      { noremap = true, silent = true }
     )
     vim.keymap.set('n', '<leader>?', builtin.oldfiles,
-    { noremap = true, silent = true }
+      { noremap = true, silent = true }
     )
     vim.keymap.set('n', '<leader>fh', builtin.help_tags,
-    { noremap = true, silent = true }
+      { noremap = true, silent = true }
     )
     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
     vim.keymap.set('n', '<leader>fw', function()
